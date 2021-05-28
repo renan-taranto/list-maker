0b2dae086de70ed6d3c964018fdd44516a1409c5
Renan Taranto <renantaranto@gmail.com>
2021-05-27 21:17:35 -0300

    [Event Source] Remove ineffective error handler

--- src/services/event-source/index.js
+++ src/services/event-source/index.js
@@ -1,14 +1,8 @@
-import store from '@/store'
-
 export default {
   create (topic, handlers) {
     const eventSource = new EventSource(
       `${process.env.VUE_APP_MERCURE_HUB_URL}/.well-known/mercure?topic=` + encodeURIComponent(topic)
     )
-
-    eventSource.onerror = () => {
-      store.dispatch('app/showError', 'An error has occurred. Please try again later.')
-    }
 
     eventSource.onmessage = (event) => {
       const data = JSON.parse(event.data)
